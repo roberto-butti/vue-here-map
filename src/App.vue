af42d3a9bd461a37a3e3bc82e43c00c284b8e003
Roberto Butti <roberto.butti@gmail.com>
2018-04-28 11:19:16 +0200

    Map View Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MapView msg="Map HERE" />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MapView from './components/MapView.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MapView
   }
 }
 </script>
